d0b7ca38ada2e422bf97eefe2667f6a4110fb1b8
Joanne <kusjoanne@gmail.com>
2025-07-21 12:27:26 -0500

    Update streamlit_app.py
    Add search on join

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,10 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
 ingredients = st.multiselect(
     "Choose up to 5 ingredients:",
     my_dataframe,
@@ -31,6 +35,10 @@ if ingredients:
 
     for fruit in ingredients:
         ingredients_string += fruit + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/" + fruit)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
