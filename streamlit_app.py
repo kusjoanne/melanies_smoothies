a088fc777237eaaa35d82b86535efafdbfc9e095
Joanne <kusjoanne@gmail.com>
2025-07-21 11:55:38 -0500

    Update streamlit_app.py
    Debug API Calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,8 @@ st.write("The name to call for your smoothie is: ", name)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients = st.multiselect(
     "Choose up to 5 ingredients:",
